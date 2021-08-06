89d78a764fc72d24d42fecf65d13b02a93610556
イゴーリ <f55dota217@gmail.com>
2021-08-06 11:00:23 +0300

    Add project files.

---
+++ VoronezhFishing/Fishes/Bondarevka/BondarevkaFish.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoronezhFishing.Fishes.Bondarevka
+{
+	abstract public class BondarevkaFish
+	{
+		public double Weight { get; set; }
+		public bool IsPredator { get; set; }
+		public string Name { get; }
+		public DateTime CatchingTime { get;}
+
+		public abstract void Catched();
+	}
+
+	public class Plotva : BondarevkaFish
+	{
+		public override void Catched()
+		{
+			
+		}
+	}
+}
